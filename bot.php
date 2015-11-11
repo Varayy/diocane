f8d77467b38b5844683fafc02edcc5c4ce4ee58b
Varayy <vareimc57@gmail.com>
2015-11-11 14:00:36 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,15 +1,38 @@
-    <?php
-
-
-    $botToken = "174148485:AAG04T-zqtDjheJRqr5VLeDAmjA162z-n7Q";
-    $website = "https://api.telegram.org/bot".$botToken;
-
-    $update = file_get_contents("php://input");
-
-    $updateArray = jason_decode($update, TRUE);
-
-    $chatId = $updateArray["result"][]["message"]["chat"]["id"];
-
-    file_get_contents($website"/sendphoto?chat_id=".$chatId."&photo=AgADBAADtqcxGyXBCQe9BhMBOgtPjjd5cjAABDJkwvAh7xfaiIsBAAEC");
-
-    ?>
+<?php
+ 
+$botToken = "155094884:AAHZ_DTJAznjxLBrCbEgivaFj2B4s2H2o-c";
+$website = "https://api.telegram.org/bot".$botToken;
+ 
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+ 
+ 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ 
+ 
+switch($message) {
+       
+        case "/fotoariana":
+                sendMessage($chatId, "diocane!");
+                break;
+        case "/testoariana":
+                sendMessage($chatId, "porcodio!");
+                break;
+        default:
+                sendMessage($chatId, "default");
+       
+}
+ 
+function sendMessage ($chatId, $message) {
+       
+        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+        file_get_contents($url);
+       
+}
+ 
+ 
+ 
+ 
+ 
+?>
